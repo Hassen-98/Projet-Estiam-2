42242a03d583c6d1d6d6145793f78294205dba00
hassen <hassen.agoun@isticbc.org>
2021-11-28 15:43:41 +0100

    authentificatin d'utilisateur

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,39 +1,43 @@
 const router = require('express').Router()
 
+const {
+     userLogin,
+     userRegister,
+    } = require("../utils/authentification");
+
 //Enregistrement utilisateur
 router.post('/register-user', async (req, res) => {
-
+    await userRegister(req.body, "user", res);
 
 })
 
 //Enregistrement administrateur
 router.post('/register-admin', async (req, res) => {
-
+    await userRegister(req.body, "admin", res);
 
 })
 
 //Enregistrement super-administrateur
 router.post('/register-superadmin', async (req, res) => {
-
+    await userRegister(req.body, "superadmin", res);
 
 })
 
 //connection utilisateur
 router.post('/login-user', async (req, res) => {
-
+    await userLogin(req.body, "user", res);
 
 })
 
 //connection administrateur
 router.post('/login-admin', async (req, res) => {
-
+    await userLogin(req.body, "admin", res);
 
 })
 
 //connection super-administrateur
 router.post('/login-superadmin', async (req, res) => {
-
-
+    await userLogin(req.body, "superadmin", res);
 })
 
 //protection utilisateur
