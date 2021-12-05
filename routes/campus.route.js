36b05aea11f023f9197c8d25a1cf7ac998650c7a
Mamadou <mamadou.sall@42c.fr>
2021-12-05 03:21:04 +0100

    protect campus

--- routes/campus.route.js
+++ routes/campus.route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const CampusController = require("../controllers/campus.controller");
 
+const { userAuth, checkRole} = require('../utils/Auth')
 
-
-router.post("/", CampusController.createCampus);
+router.post("/", userAuth, checkRole(["superadmin","admin"]),CampusController.createCampus);
 router.get("/all", CampusController.getAllcampus);
 router.get("/:id", CampusController.getByIdCampus);
-router.patch("/:id", CampusController.updateCampus);
-router.delete("/:id",CampusController.deleteCampus);
+router.patch("/:id", userAuth, checkRole(["superadmin","admin"]),CampusController.updateCampus);
+router.delete("/:id",userAuth, checkRole(["superadmin","admin"]),CampusController.deleteCampus);
 module.exports = router;
