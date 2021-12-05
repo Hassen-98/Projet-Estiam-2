c4562767b827b26ea93081c1e0bc1517a4859e1b
Mamadou <mamadou.sall@42c.fr>
2021-12-05 16:42:41 +0100

    protect get all and get by id user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,13 @@ const { userRegister, userLogin, userAuth, serializeUser, checkRole} = require('
 
 
 //CRUD USERS
-router.get("/all",  userController.getAllUsers); 
-router.get("/:id", userController.getByIdUser); 
+router.get("/all",  userAuth, checkRole(["superadmin","admin"]),userController.getAllUsers); 
+router.get("/:id", userAuth, checkRole(["superadmin","admin"]),userController.getByIdUser); 
 router.patch("/:id", userAuth, checkRole(["superadmin","admin"]),userController.updateUser); 
-router.delete("/:id", userAuth, checkRole(["superadmin"]), userController.deleteUser);
+router.delete("/:id", userAuth, checkRole(["superadmin","admin"]), userController.deleteUser);
 
 //Enregistrement admin et superadmin
-router.post('/signup/superadmin', async (req, res) => {
+router.post('/signup/superadmin', userAuth, checkRole(["superadmin"]),async (req, res) => {
     await userRegister(req.body,"superadmin", res);
 })
 
