194c27e67928a44fde81ad4ecc209e55ce9588b1
Mamadou <mamadou.sall@42c.fr>
2021-12-05 03:21:41 +0100

    protect entreprise

--- routes/entreprise.route.js
+++ routes/entreprise.route.js
@@ -2,12 +2,13 @@ const router = require('express').Router()
 
 const EntrepriseController = require("../controllers/entreprise.controller");
 
+const { userAuth, checkRole} = require('../utils/Auth')
 
 //CRUD Program
-router.post("/", EntrepriseController.createEntreprise);
+router.post("/", userAuth, checkRole(["superadmin","admin"]),EntrepriseController.createEntreprise);
 router.get("/all", EntrepriseController.getAllEntreprise);
 router.get("/:id", EntrepriseController.getByIdEntreprise);
-router.patch("/:id", EntrepriseController.updateEntrprise);
-router.delete("/:id", EntrepriseController.deleteEntreprise);
+router.patch("/:id", userAuth, checkRole(["superadmin","admin"]),EntrepriseController.updateEntrprise);
+router.delete("/:id", userAuth, checkRole(["superadmin","admin"]),EntrepriseController.deleteEntreprise);
 
 module.exports = router;
